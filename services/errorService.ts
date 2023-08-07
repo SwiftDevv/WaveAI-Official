8d42e11860645f7f2163901271754cba80d14282
WaveAI <139340692+SwiftDevv@users.noreply.github.com>
2023-08-07 23:17:28 +0530

    Update errorService.ts

--- services/errorService.ts
+++ services/errorService.ts
@@ -13,17 +13,13 @@ const useErrorService = () => {
         return !error
           ? null
           : ({
-              title: t('Error fetching models.'),
+              title: t(''),
               code: error.status || 'unknown',
               messageLines: error.statusText
-                ? [error.statusText]
+                ? ['']
                 : [
-                    t(
-                      'Make sure your OpenAI API key is set in the bottom left of the sidebar.',
-                    ),
-                    t(
-                      'If you completed this step, OpenAI may be experiencing issues.',
-                    ),
+                    t(''),
+                    t(''),
                   ],
             } as ErrorMessage);
       },
